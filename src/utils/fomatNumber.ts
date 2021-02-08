860e500b3c2092644aeb18b9f508147c24f78f28
Sotatek-DatNguyen4 <dat.nguyen4@sotatek.com>
2021-02-08 09:57:16 +0700

    clean code

--- src/utils/fomatNumber.ts
+++ src/utils/fomatNumber.ts
@@ -1,6 +1,6 @@
 import BigNumber from 'bignumber.js';
 
-export function formatPrecisionAmount(amount: any, precision: number = 18): string {
+export const formatPrecisionAmount = (amount: any, precision: number = 18) => {
   const rawValue = new BigNumber(`${amount}`).toFixed(precision);
   return (amount && parseFloat(amount) !== Infinity) ? new BigNumber(rawValue).toFormat() : '0';
-}
+};
