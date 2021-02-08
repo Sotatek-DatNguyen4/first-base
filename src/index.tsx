f545e729382b79125b41c37a0eb8a2e081f50a09
Sotatek-DatNguyen4 <dat.nguyen4@sotatek.com>
2021-02-08 09:28:44 +0700

    clean code

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/common.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/common.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 reportWebVitals();
