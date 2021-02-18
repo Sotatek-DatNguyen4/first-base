754429e4a851c06258f263b7d0ea5ea3bfc9e23d
Sotatek-DatNguyen4 <dat.nguyen4@sotatek.com>
2021-02-18 08:49:20 +0700

    update routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, withRouter, RouteComponentProps } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { clearAlert } from '../store/actions/alert';
+import Error from '../pages/error';
+import NotFound from '../pages/not-found';
+import ErrorBoundary from '../components/common/error-boundary';
+import Home from '../pages/home';
+import Login from '../pages/login';
+import Form from '../pages/form';
+
+//@ts-ignore
+import { NotificationContainer, NotificationManager } from 'react-notifications';
+import 'react-notifications/lib/notifications.css';
+import AppContainer from '../AppContainer';
+
+const Routes: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
+  const dispatch = useDispatch();
+
+  const alert = useSelector((state: any) => state.alert);
+  const { history } = props;
+
+  useEffect(() => {
+    const { type, message } = alert;
+    if (type && message) {
+      NotificationManager[type](message);
+    }
+  }, [alert]);
+
+  useEffect(() => {
+    history.listen((location, action) => {
+      dispatch(clearAlert());
+    });
+  }, []);
+
+
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/error" component={Error} />
+        <Route path="/login" component={Login} />
+        <Route path="/form" component={Form} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  )
+};
+
+const RoutesHistory = withRouter(Routes);
+
+const routing = function createRouting() {
+  return (
+    <>
+      <NotificationContainer />
+      <Router>
+        <AppContainer>
+          <ErrorBoundary>
+            <RoutesHistory />
+          </ErrorBoundary>
+        </AppContainer>
+      </Router>
+    </>
+  );
+};
+
+export default routing;
